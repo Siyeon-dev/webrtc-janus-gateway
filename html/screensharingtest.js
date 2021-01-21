bd77fe7a64278c56ded954612abe88c7384dcf94
SiyeonPark <siyeon.dev@gmail.com>
2021-01-21 17:28:02 +0900

    connect IP Adress with AWS Server

--- html/screensharingtest.js
+++ html/screensharingtest.js
@@ -44,10 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+	server = "http://" +  "54.180.150.241" + "/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
-
+	server = "https://" + "54.180.150.241" + "/janus";
 
 var janus = null;
 var screentest = null;
