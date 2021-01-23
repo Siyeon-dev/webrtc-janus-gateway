459a2fef1e894b455df94129ba6f1c46ce55ae47
SiyeonPark <siyeon.dev@gmail.com>
2021-01-23 13:57:51 +0900

    Update remoteFeed.createAnswer audio and video send in media attribute

--- html/videoroomtest.js
+++ html/videoroomtest.js
@@ -549,7 +549,7 @@ function newRemoteFeed(id, display, audio, video) {
 							jsep: jsep,
 							// Add data:true here if you want to subscribe to datachannels as well
 							// (obviously only works if the publisher offered them in the first place)
-							media: { audioSend: false, videoSend: false },	// We want recvonly audio/video
+							media: { audioSend: true, videoSend: true },	// We want recvonly audio/video
 							success: function(jsep) {
 								Janus.debug("Got SDP!", jsep);
 								var body = { request: "start", room: myroom };
