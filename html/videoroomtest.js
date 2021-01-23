c9a95f272f9d24e1d873375b377a8ac6caccdbf7
SiyeonPark <siyeon.dev@gmail.com>
2021-01-23 11:03:34 +0900

    delete server port num

--- html/videoroomtest.js
+++ html/videoroomtest.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" +  window.location.hostname + ":8088/janus";
+	server = "http://" +  "re-coder.net" + "/janus";
 else
-	server = "https://" + window.location.hostname + ":8088/janus";
+	server = "https://" + "re-coder.net" + "/janus";
 
 var janus = null;
 var sfutest = null;
