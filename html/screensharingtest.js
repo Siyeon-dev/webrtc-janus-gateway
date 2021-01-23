d8be2ae447cd283b6484055d1310e2fb7f93961f
SiyeonPark <siyeon.dev@gmail.com>
2021-01-23 10:52:17 +0900

    modify server address

--- html/screensharingtest.js
+++ html/screensharingtest.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" +  window.location.hostname + "/janus";
+	server = "http://" +  window.location.hostname + ":8088/janus";
 else
-	server = "https://" + window.location.hostname + "/janus";
+	server = "https://" + window.location.hostname + ":8088/janus";
 
 var janus = null;
 var screentest = null;
